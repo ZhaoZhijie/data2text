cd5fccca6e553d89b4786246e2c79fdb016bdff5
ZhaoZhijie <ZhaoZhijie@github.com>
2020-06-14 17:22:02 +0100

    add dropout for encoder

--- onmt/encoders/ff_encoder.py
+++ onmt/encoders/ff_encoder.py
@@ -4,22 +4,29 @@ from onmt.modules import MultiHeadedAttention
 
 
 class FeedForwardEncoderLayers(torch.nn.Module):
-  def __init__(self, input_size, units, layers_num):
-    super(FeedForwardEncoderLayers, self).__init__()
-    self.layers = torch.nn.ModuleList()
-    self.layers_num = layers_num
-    self.activate = torch.nn.ReLU()
-    input_size = input_size
-    for i in range(layers_num):
-      self.layers.append(torch.nn.Linear(input_size, units))
-      input_size = units
+    def __init__(self, input_size, units, layers_num, dropout=0.1):
+        super(FeedForwardEncoderLayers, self).__init__()
+        self.layers = torch.nn.ModuleList()
+        self.dropout = torch.nn.ModuleList()
+        self.layers_num = layers_num
+        self.activate = torch.nn.ReLU()
+        input_size = input_size
+        for i in range(layers_num):
+            self.layers.append(torch.nn.Linear(input_size, units))
+            self.dropout.append(torch.nn.Dropout(dropout))
+        input_size = units
 
-  def forward(self, x):
-    for i in range(self.layers_num):
-      x = self.layers[i](x)
-      if i != self.layers_num - 1:
-        x = self.activate(x)
-    return x
+    def forward(self, x):
+        for i in range(self.layers_num):
+            x = self.layers[i](x)
+            if i != self.layers_num - 1:
+                x = self.activate(x)
+            x = self.dropout[i](x)
+        return x
+
+    def update_dropout(self, dropout):
+        for i in range(self.layers_num):
+            self.dropout[i].p = dropout
 
 def pad_mask(src, pad_idx):
     return torch.eq(src, pad_idx)
@@ -42,11 +49,11 @@ class FeedForwardEncoder(EncoderBase):
         * memory_bank ``(src_len, batch_size, model_dim)``
     """
 
-    def __init__(self, num_layers, size, heads, embeddings):
+    def __init__(self, num_layers, size, heads, embeddings, dropout):
         super(FeedForwardEncoder, self).__init__()
         self.embeddings = embeddings
         self.size = size
-        self.encode_layers = FeedForwardEncoderLayers(embeddings.embedding_size, size, num_layers)
+        self.encode_layers = FeedForwardEncoderLayers(embeddings.embedding_size, size, num_layers, dropout)
         # self.attention = MultiHeadedAttention(heads, embeddings.embedding_size)
 
     @classmethod
@@ -56,7 +63,8 @@ class FeedForwardEncoder(EncoderBase):
             opt.enc_layers,
             opt.enc_rnn_size,
             opt.heads,
-            embeddings)
+            embeddings,
+            opt.dropout)
 
     def forward(self, src, lengths=None):
         """See :func:`EncoderBase.forward()`"""
@@ -77,6 +85,7 @@ class FeedForwardEncoder(EncoderBase):
 
     def update_dropout(self, dropout):
         self.embeddings.update_dropout(dropout)
+        self.encode_layers.update_dropout(dropout)
 
 
 
