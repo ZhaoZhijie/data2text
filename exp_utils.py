aa1696fcff8efb6214080882170795c658e386ce
ZhaoZhijie <ZhaoZhijie@github.com>
2020-08-02 02:54:23 +0100

    fix continued model name error

--- exp_utils.py
+++ exp_utils.py
@@ -93,7 +93,7 @@ def get_continued_models(seeds, exps):
         for exp in exps:
             step = get_last_step(seed, exp)
             tag = prepare_common_model(seed, exp, step//1000)
-            path = "experiments/exp-seed-{}/exp-{}/models/model_step_{}".format(seed, exp, step)
+            path = "experiments/exp-seed-{}/exp-{}/models/model_step_{}.pt".format(seed, exp, step)
             fw.write(path+"\n")
     fw.close()
 
