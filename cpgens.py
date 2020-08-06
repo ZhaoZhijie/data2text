1ffeaf3d93c028193b689ad40bc3aa580454df59
ZhaoZhijie <ZhaoZhijie@github.com>
2020-08-06 10:30:53 +0100

    cpgens.py add seeds

--- cpgens.py
+++ cpgens.py
@@ -2,16 +2,17 @@ import re
 import os
 
 
-seeds = [123, 528, 287, 684, 963]
+seeds = [123,168,234,287,451,528,684,737,835,963]
 exps = ["S1D1", "S1D2", "S4D1", "S4D2"]
 
 tar_folder = "genfiles"
 
 for seed in seeds:
     for exp in exps:
-        gen_folder = "experiments/exp-seed-{}/exp-{}/gens/*".format(seed, exp)
+        gen_folder = "experiments/exp-seed-{}/exp-{}/gens/".format(seed, exp)
         copy_foler = "{}/experiments/exp-seed-{}/exp-{}/gens/".format(tar_folder, seed, exp)
         os.makedirs(copy_foler)
+
         cmd = "cp -r {} {}".format(gen_folder, copy_foler)
         os.system(cmd)
 
